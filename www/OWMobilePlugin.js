c1c5a07f0338125f046feeb6fa31be3838741bc5
apalmaPL <apalma@product-league.com>
2025-04-29 14:19:01 +0200

    Update OWMobilePlugin.js

--- www/OWMobilePlugin.js
+++ www/OWMobilePlugin.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'OWMobilePlugin', 'coolMethod', [arg0]);
+exports.login = function (success, error) {
+  exec(success, error, 'OneWelcomePlugin', 'login', []);
+};
+
+exports.logout = function (success, error) {
+  exec(success, error, 'OneWelcomePlugin', 'logout', []);
+};
+
+exports.register = function (success, error) {
+  exec(success, error, 'OneWelcomePlugin', 'register', []);
 };

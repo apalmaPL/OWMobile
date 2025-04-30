ee95bc41179f9e6f193b5c0849e3c7cdec090547
apalmaPL <apalma@product-league.com>
2025-04-30 15:59:54 +0200

    Update OWMobilePlugin.js

--- www/OWMobilePlugin.js
+++ www/OWMobilePlugin.js
@@ -1,45 +1,44 @@
-/**
- * OWMobilePlugin
- * @constructor
- */
-function OWMobilePlugin(pluginName) {
-    this.version = 1;
-    this.pluginName = pluginName;
-}
 
+var OWMobilePlugin = (function(){
+    
+    var OWMobilePlugin = {};
 
-/**
- * login using OW
- * @param successCallback
- * @param errorCallback
- */
-OWMobilePlugin.prototype.login = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, this.pluginName, 'login', []);
-};
-
-/**
- * logout using OW
- * @param successCallback
- * @param errorCallback
- */
-OWMobilePlugin.prototype.logout = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, this.pluginName, 'logout', []);
-};
-
-/**
- * register using OW
- * @param username
- * @param password
- * @param successCallback
- * @param errorCallback
- */
-OWMobilePlugin.prototype.register = function(username, password, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, this.pluginName, 'register', [username, password]);
-};
+    /**
+     * login using OW
+     * @param successCallback
+     * @param errorCallback
+     */
+    OWMobilePlugin.prototype.login = function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'OWMobilePlugin', 'login', []);
+    };
+    
+    /**
+     * logout using OW
+     * @param successCallback
+     * @param errorCallback
+     */
+    OWMobilePlugin.prototype.logout = function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'OWMobilePlugin', 'logout', []);
+    };
+    
+    /**
+     * register using OW
+     * @param username
+     * @param password
+     * @param successCallback
+     * @param errorCallback
+     */
+    OWMobilePlugin.prototype.register = function(username, password, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, 'OWMobilePlugin', 'register', [username, password]);
+    };
+        
+    
+    return OWMobilePlugin;
+});
 
 
 /**
  * export default OWMobilePlugin
  * @type {OWMobilePlugin}
  */
-module.exports = new OWMobilePlugin('OWMobilePlugin');
+module.exports = new OWMobilePlugin();
